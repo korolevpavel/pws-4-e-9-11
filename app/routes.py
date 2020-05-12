78a4c4612541301c9f2a6ea5b771036f384574d0
Korolev Pavel <korolev.vrn@gmail.com>
2020-05-12 13:59:58 +0300

    add update event

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 import datetime
 import logging
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     events = Event.query.all()
@@ -81,7 +82,29 @@ def add_event():
             return redirect("/")
     return render_template("add_event.html", form=form, date_start=datetime.datetime.now())
 
+
+@app.route('/edit/<int:_id>', methods=["GET", "POST"])
+def edit_event(_id):
+    event = Event.query.filter(Event._id == int(_id)).first()
+    form = EventForm(obj=event)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            subject = request.form.get('subject')
+            description = request.form.get('description')
+            date_start = request.form.get('date_start')
+            date_end = request.form.get('date_end')
+            event.subject = subject
+            event.description = description
+            event.date_start = date_start
+            event.date_end = date_end
+            event.author = current_user.get_id()
+            db.session.commit()
+        return redirect("/")
+    return render_template("add_event.html", form=form, date_start=datetime.datetime.now())
+
+
 @app.route("/my_events", methods=["GET", "POST"])
 def my_events():
-    events = Event.query.filter(Event.author==current_user.get_id())
-    return render_template('index.html', events=events, current_user=current_user)
+    if request.method == 'GET':
+        events = Event.query.filter(Event.author == current_user.get_id())
+        return render_template('index.html', events=events, current_user=current_user)
